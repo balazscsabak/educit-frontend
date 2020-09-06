b69db257ff6c96a19011a291a3439fd0411b88f5
balazscsabak <balazscsabak93@gmail.com>
2020-09-06 15:34:16 +0200

    changed layout comp

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import FeaturedPosts from '../components/FeaturedPosts/Index'
 import { categoriesData } from '../utils/constans'
 import CatSection from '../components/CatSection/CatSection'
