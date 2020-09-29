c7989d128742ab25723726e2e36a33d08c55ad92
balazscsabak <balazscsabak93@gmail.com>
2020-09-29 12:27:11 +0200

    added adsense

--- pages/_document.js
+++ pages/_document.js
@@ -9,23 +9,28 @@ export default class CustomDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx)
 
     return {
-      ...initialProps
+      ...initialProps,
     }
   }
 
   render() {
     return (
-      <html lang='hu'>
+      <html lang="hu">
         <Head>
           <script
-            data-ad-client='ca-pub-4026486048166934'
+            data-ad-client="ca-pub-4026486048166934"
             async
-            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script>
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
+          <script
+            data-ad-client="ca-pub-4026486048166934"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -36,7 +41,7 @@ export default class CustomDocument extends Document {
                     gtag('config', '${GA_TRACKING_ID}', {
                       page_path: window.location.pathname,
                     });
-                  `
+                  `,
             }}
           />
         </Head>
